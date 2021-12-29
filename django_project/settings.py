052b1c6982653bf75395d8c98d8e323bf4c628a8
Adams Mohammed <moadams847@gmail.com>
2021-12-29 15:17:47 +0000

    add runtime.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -146,7 +146,6 @@ STATICFILES_DIRS = (
 PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, "templates"),
-    # here you can add another templates directory if you wish.
 )
 
 
